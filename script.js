5834c1dc4051eff09744fc657b769713ee8fb5ae
Wi1mer <wilmerdiazolaya@gmail.com.com>
2021-05-20 20:49:41 -0500

    V-1.0

---
+++ script.js
@@ -0,0 +1,9 @@
+
+	
+<!--Recargar la paguina-->
+<script type="text/javascript">
+  //Función actualizar
+function actualizar(){location.reload(true);}
+//Función para actualizar cada 5 segundos(5000 milisegundos)
+setInterval("actualizar()",1800000);
+</script>
